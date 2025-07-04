cf68dd2d2ae7293bba183229cc46a913228ba314
Aditya Kanu <adityakanu1st@gmail.com>
2025-07-05 00:09:08 +0530

    added scripts to build registry

---
+++ registry/registry-components.ts
@@ -0,0 +1,11 @@
+import { Registry } from "./schema";
+
+export const ui: Registry = [
+  {
+    name: "button",
+    type: "registry:ui",
+    files: [
+      "../components/memphi/button.tsx",
+    ],
+  }
+];
